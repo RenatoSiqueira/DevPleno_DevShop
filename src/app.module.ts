4f4e07ea1b140432464111391f3d9821c3919a0f
renatosiqueira <renatoelysiqueira@gmail.com>
2021-01-26 00:24:42 -0300

    .env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { async } from 'rxjs'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { Category } from './category/category.entity'
@@ -8,14 +10,26 @@ import { CategoryModule } from './category/category.module'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: 'postgres://postgres:senha@localhost:5432/devshop',
-      autoLoadEntities: true,
-      synchronize: true,
-      // entities: [Category],
-      logging: true
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true
+      })
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   url: '',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   // entities: [Category],
+    //   logging: true
+    // }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
