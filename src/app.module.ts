ff340e2c36cbfed25dbd5c139fe347ae1790a533
renatosiqueira <renatoelysiqueira@gmail.com>
2021-01-26 00:14:39 -0300

    Entity and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { Category } from './category/category.entity'
 import { CategoryModule } from './category/category.module'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:senha@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+      // entities: [Category],
+      logging: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
